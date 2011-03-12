a62a1a0cbaddc80fcee27a794789a471b86d9407
Pavlin Angelov <fonix.simmol@gmail.com>
2011-03-12 23:46:33 +0200

    Django tutorial end; After refactor code to use generic views;

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 # Models imports
 from polls.models import Poll, Choice
 
+"""
 def index(request):
   latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 
@@ -18,7 +19,7 @@ def detail(request, poll_id):
                              {'poll':poll},
                              context_instance=RequestContext(request)
                             )
-
+"""
 def results(request, poll_id):
   poll = get_object_or_404(Poll, pk=poll_id)
   return render_to_response('polls/results.html', {'poll' : poll})
@@ -30,12 +31,12 @@ def vote(request, poll_id):
     selected_choice = poll.choice_set.get(pk=request.POST['choice'])
   except (KeyError, Choice.DoesNotExist):
     # Redisplay the poll voting form
-    return render_to_response('polls/details.html',
-                              {'poll': poll, 'error_message': "You didn't select a choice"},
+    return render_to_response('polls/poll_detail.html',
+                              {'object': poll, 'error_message': "You didn't select a choice"},
                               context_instance=RequestContext(request)
                              )
   else:
     selected_choice.votes += 1
     selected_choice.save()
 
-  return HttpResponseRedirect(reverse('polls.views.results', args=(poll.id,)))
+  return HttpResponseRedirect(reverse('poll_results', args=(poll.id,)))
